b9b9988cd937104e56961622cfb8b3b3cc81d9aa
iancooper <ian_hammond_cooper@yahoo.co.uk>
2020-08-28 22:14:15 +0100

    Step 6: fake it 'til you make it on rows __str__

--- conway/src/conway/game_of_life.py
+++ conway/src/conway/game_of_life.py
@@ -7,15 +7,30 @@ Notes:
 from typing import Any
 
 class Board:
-    def __init__(self, generation:int, rows:int, cols:int):
-        self.rows = rows
-        self.cols = cols
+    def __init__(self, generation:int, size:'Size'):
+        self.rows = size.rows
+        self.cols = size.cols
         self.generation = generation
 
-        self ._rows = [Row(cols) for n in range(self.rows)]
+        self ._rows = [Row(self.cols) for n in range(self.rows)]
 
     def __getitem__(self, key):
         return self._rows[key]
+
+    def __str__(self):
+        view = []
+        view.append(f"Generation {self.generation}")
+        view.append(str(Size))
+        for r in range(self.rows):
+            grid.append(str(self[r]))
+
+class Size:
+    def __init__(self, rows: int, cols:int):
+        self.rows = rows
+        self.cols = cols
+
+    def __str__(self):
+        pass
 
 class Row:
     def __init__(self, cols: int):
@@ -25,7 +40,11 @@ class Row:
     def __getitem__(self, key):
         return self._cells[key]
 
+    def __str__(self):
+        return "...."
+
 class Cell:
     def __str__(self):
         return "."
+
 
--- conway/tests/test_game_of_life.py
+++ conway/tests/test_game_of_life.py
@@ -4,11 +4,11 @@ Date: 28 August 2020
 Notes:
 """
 
-from conway import Board, Cell, Row
+from conway import Board, Cell, Row, Size
 
 
 def test_we_can_create_a_board():
-    board = Board(0, 3, 3)
+    board = Board(0, Size(3, 3))
 
     for r in range(board.rows):
         for j in range(board.cols):
@@ -27,8 +27,41 @@ def test_we_can_read_a_cell():
 
     assert str(cell) == "."
 
+
 def test_a_board_has_a_gen_identifier():
-    board = Board(0, 3,3 )
+    board = Board(0, Size(3, 3))
     assert board.generation == 0
+
+
+def test_a_size_has_two_dimensions():
+    size = Size(3, 4)
+    size.rows = 3
+    size.cols = 4
+
+
+def test_render_a_board():
+    board = Board(0, Size(3, 4))
+    expected_board = ("Generation 0\n"
+                      "3 4\n"
+                      "....\n"
+                      "....\n"
+                      "....\n"
+                      )
+
+    assert expected_board == str(board)
+
+
+def test_render_a_row_with_four_cols():
+    row = Row(4)
+    expected_row = "...."
+
+    assert expected_row == str(row)
+
+
+def test_render_a_row_with_five_cols():
+    row = Row(5)
+    expected_row = "....."
+
+    assert expected_row == str(row)
 
 # Test that a cell can be live too
