c9b7dd424cab72a7cdc3dcec94d82f70f1402bca
iancooper <ian_hammond_cooper@yahoo.co.uk>
2020-08-28 18:34:50 +0100

    step one, create a board with a size

--- conway/src/conway/game_of_life.py
+++ conway/src/conway/game_of_life.py
@@ -4,8 +4,12 @@ Date: 28 August 2020
 Notes:
 """
 
+from typing import Any
 
 class Board:
-    def __init__(self, rows, cols):
+    def __init__(self, rows:int, cols:int):
         self.rows = rows
         self.cols = cols
+
+    def __getitem__(self, key):
+        return []
--- conway/tests/test_game_of_life.py
+++ conway/tests/test_game_of_life.py
@@ -9,8 +9,11 @@ from conway import Board
 def test_we_can_create_a_board():
     board = Board(3, 3)
 
-    assert board.rows == 3
-    assert board.cols == 3
+    for r in range(board.rows):
+        for j in range(board.cols):
+            assert str(board[r][j]) == "*"
+
+
 
 
 
